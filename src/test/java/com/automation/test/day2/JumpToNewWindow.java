47b66d1ce1fae5bf1c162ffadb1e7b80959ecc94
TatsianaDabko <dobkotanya08@gmail.com>
2020-03-03 14:08:02 -0500

    created basic navigation class1

---
+++ src/test/java/com/automation/test/day2/JumpToNewWindow.java
@@ -0,0 +1,16 @@
+package com.automation.test.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JumpToNewWindow {
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tap");
+        Thread.sleep(5000);
+        String windowHandle = driver.getWindowHandle();
+        System.out.println(windowHandle);
+    }
+}
---
+++ src/test/java/com/automation/test/day2/QuitVSclose.java
@@ -0,0 +1,16 @@
+package com.automation.test.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class QuitVSclose {
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/open_new_tap");
+        Thread.sleep(4000);
+      //  driver.close();
+        driver.quit();
+    }
+}
