526cf1529e4a89cb0b5b3c5a164314b7f217459c
TatsianaDabko <dobkotanya08@gmail.com>
2020-03-03 12:23:54 -0500

    created basic navigation class1

--- src/test/java/com/automation/test/day2/BasicNavigation.java
+++ src/test/java/com/automation/test/day2/BasicNavigation.java
@@ -4,10 +4,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://google.com");
+        Thread.sleep(3000);
+        String title = driver.getTitle();
+        String expectedTitle = "Google";
+        System.out.println("Title is..." +title);
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+        driver.close();
 
 
     }
