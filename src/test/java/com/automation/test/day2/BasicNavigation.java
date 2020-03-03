ff2975b0c62e738acb73e440c01d317ccce6e2ea
TatsianaDabko <dobkotanya08@gmail.com>
2020-03-03 11:28:59 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/test/day2/BasicNavigation.java
@@ -0,0 +1,13 @@
+package com.automation.test.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+    }
+}
