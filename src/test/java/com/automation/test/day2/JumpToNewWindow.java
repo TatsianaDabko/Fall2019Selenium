378aeb8267c763525eaef39e4f337e3019f26bea
TatsianaDabko <dobkotanya08@gmail.com>
2020-03-03 15:23:24 -0500

    created basic navigation class1

--- src/test/java/com/automation/test/day2/JumpToNewWindow.java
+++ src/test/java/com/automation/test/day2/JumpToNewWindow.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToNewWindow {
     public static void main(String[] args) throws Exception {
         WebDriverManager.chromedriver().setup();
@@ -12,5 +14,26 @@ public class JumpToNewWindow {
         Thread.sleep(5000);
         String windowHandle = driver.getWindowHandle();
         System.out.println(windowHandle);
+        Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println(windowHandles);
+        System.out.println("URL before switch"+ driver.getCurrentUrl());
+
+        for (String windowId: windowHandles){
+            if(!windowId.equals(windowHandle)){
+                driver.switchTo().window(windowId);
+            }
+        }
+        System.out.println("URL after switch"+ driver.getCurrentUrl());
+        driver.close();
+    }
+
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for(String window: windows){
+            driver.switchTo().window(window);
+            if(driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
     }
 }
